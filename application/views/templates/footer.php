f9a628c587e0fffe10a22b66c76336da0d212730
Nicholas Ruscillo <nic@285solutions.com>
2017-12-29 15:57:06 -0500

    including GA scripting reference and separate footer links more

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,12 +6,14 @@
                 <div class="text-left col-6">ClicksPicks &copy; Copyright <?= date("Y") ?>. All rights reserved.</div>
                 <div class="text-right col-6">
                     <a href="privacy" target="_blank">Privacy Policy</a>
+                    &nbsp;|&nbsp;
                     <a href="terms" target="_blank">Terms and Conditions</a>
+                    &nbsp;|&nbsp;
                     <a href="login" target="_blank">Login</a>
                 </div>
             </div>
         </footer>  
-    <div> <!-- container  -->
-    <script type="text/javascript" src="<?= base_url('assets/js/google_analytics.js') ?>" ></script>
+    </div> <!-- container  -->
+    <script type="text/javascript" src="<?= base_url("assets/js/google_analytics.js") ?>"></script>
 </body>
 </html>
