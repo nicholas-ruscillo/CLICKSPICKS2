3a0b58ad45733d27716db0075634fb5799977121
Nicholas Ruscillo <nic@285solutions.com>
2017-12-29 16:01:04 -0500

    adding data elements specific to kids page

--- application/controllers/Kids.php
+++ application/controllers/Kids.php
@@ -4,12 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Kids extends CI_Controller {
     public function index(){
         $this->load->helper('url');
-        $this->load->model('AnnouncementModel');                
+        //$this->load->model('AnnouncementModel');                
         
-        $data['announcements'] = $this->AnnouncementModel->index();
-        $data['title'] = 'Welcome to West Rouge Junior Public School';
-        $data['description'] = 'Your central source for everything related to West Rouge Junior Public School - West Rouge, Ontario, Canada';
-        $data['keywords'] = 'wrjp, wrjps, west rouge jp, west rouge jps, west rouge junior ps, west rouge junior public school';
+        //$data['announcements'] = $this->AnnouncementModel->index();
+        $data['title'] = 'Your central jump-point for kid-safe games, education and interest websites - by ClicksPicks';
+        $data['description'] = 'There is no need to try and remember all the URLs of the sites your kids love - ClicksPicks does it for you. Simply click on the pre-verified kid-friendly site name to open a new window and instantly be taken to that site.';
+        $data['keywords'] = 'kid-friendly web sites, kid-safe web sites, kid-friendly URLs, kid-safe URLs, kids-safe preverified websites, central jump point for kid-safe web sites';
+        $data['css'] = "kids";
         
         $this->load->view('templates/header', $data);
         $this->load->view('pages/kids', $data);
