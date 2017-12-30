4134587f48feabe748ceda251a5425bb4cfecb27
Nicholas Ruscillo <nic@285solutions.com>
2017-12-30 09:51:46 -0500

    renaming page to index since all controllers will be referencing this page

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,8 +1,8 @@
         <!-- Jumbotron -->
         <div class="jumbotron">
-          <h1>Marketing stuff!</h1>
-          <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-          <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+          <h2>Bookmark ClicksPicks.com and Return Often!</h2>
+          <p class="lead" style="text-align:justify"><?= $marketing ?></p>
+          <p><a class="btn btn-sm btn-success" href="#" role="button">Add Your Favourite Sites</a></p>
         </div>
 
         <!-- Example row of columns -->
