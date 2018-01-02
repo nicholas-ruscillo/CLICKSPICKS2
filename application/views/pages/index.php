3058b5aefb91ee649741c8518abaf72e466e3bc1
Nicholas Ruscillo <nic@285solutions.com>
2018-01-02 17:21:26 -0500

    building display table for outputted data

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -7,17 +7,46 @@
 
         <!-- Example row of columns -->
         <div id="main_content" class="row" style="margin-bottom:1em;">
-          <div class="col-lg-2">
+          <div class="col-lg-2 text-md-center">
             <h2>Heading</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
             <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
           </div>
           <div class="col-lg-8">
             <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Website</th>
+                        <th class="text-right">Clicks</th>
+                        <th class="text-right">Comments</th>
+                        <th class="text-right">Likes</th>
+                        <th class="text-right">Dislikes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    <?php
+                    $rank = 1;
+                    foreach( $picks as $pick ){
+                        echo '<tr>';
+                        echo '<td>' . $rank . '</td>';
+                        echo '<td><a href="' . $pick['url'] . '" target="_blank">' . $pick['title'] . '</a></td>';
+                        echo '<td class="text-right">' . $pick['clicks'] . '</td>';
+                        echo '<td class="text-right">' . $pick['comments'] . '</td>';
+                        echo '<td class="text-right">' . $pick['positive'] . '</td>';
+                        echo '<td class="text-right">' . $pick['negative'] . '</td>';                        
+                        echo '</tr>';
+                        
+                        $rank++;
+                    }
+                    ?>
+                    
+                </tbody>
+            </table>
           </div>
-          <div class="col-lg-2">
+          <div class="col-lg-2 text-md-center">
             <h2>Heading</h2>
             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
             <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
