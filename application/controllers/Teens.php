56d167c0c62acfe1629f6ced0f4d393eed892c3e
Nicholas Ruscillo <nic@285solutions.com>
2018-01-02 17:21:02 -0500

    including model loading in controllers

--- application/controllers/Teens.php
+++ application/controllers/Teens.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Teens extends CI_Controller {
     public function index(){
         $this->load->helper('url');
-        //$this->load->model('AnnouncementModel');                
+        $this->load->model('TeensModel');                
         
         //$data['announcements'] = $this->AnnouncementModel->index();
         $data['title'] = 'Your central jump-point for teen-safe games, education and interest websites - by ClicksPicks';
@@ -12,6 +12,8 @@ class Teens extends CI_Controller {
         $data['keywords'] = 'teen-friendly web sites, teen-safe web sites, teen-friendly URLs, teen-safe URLs, teen-safe preverified websites, central jump point for teen-safe web sites';
         $data['marketing'] = 'You only need to remember (or bookmark) one URL to access all your teen\'s favourite websites for games, education and interests. Each website has been pre-screened to ensure a teen-safe experience so you can feel confident about your child\'s time online (adult supervision is still highly recommended). Click on any of the links below to be instantly taken to that website (the most popular sites float to the top of the list). Read or submit comments about websites to your benefit or the community by clicking on the comment link beside each website; you can also see or submit approvals/disapprovals for any site. Lastly, you can submit you own favourite sites for consideration in being added to the list - it will be pre-screened for approval before being added. Have fun and enjoy!';
         $data['css'] = "teens";
+        
+        $data['picks'] = $this->TeensModel->index();
         
         $this->load->view('templates/header', $data);
         $this->load->view('pages/index', $data);
--- application/controllers/Youths.php
+++ application/controllers/Youths.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Youths extends CI_Controller {
     public function index(){
         $this->load->helper('url');
-        //$this->load->model('AnnouncementModel');                
+        $this->load->model('YouthsModel');                
         
         //$data['announcements'] = $this->AnnouncementModel->index();
         $data['title'] = 'Your central jump-point for youth-safe games, education and interest websites - by ClicksPicks';
@@ -12,6 +12,8 @@ class Youths extends CI_Controller {
         $data['keywords'] = 'youth-friendly web sites, youth-safe web sites, youth-friendly URLs, youth-safe URLs, youth-safe preverified websites, central jump point for youth-safe web sites';
         $data['marketing'] = 'You only need to remember (or bookmark) one URL to access all your youth\'s favourite websites for games, education and interests. Each website has been pre-screened to ensure a youth-safe experience so you can feel confident about your child\'s time online (adult supervision is still highly recommended). Click on any of the links below to be instantly taken to that website (the most popular sites float to the top of the list). Read or submit comments about websites to your benefit or the community by clicking on the comment link beside each website; you can also see or submit approvals/disapprovals for any site. Lastly, you can submit you own favourite sites for consideration in being added to the list - it will be pre-screened for approval before being added. Have fun and enjoy!';
         $data['css'] = "youths";
+
+        $data['picks'] = $this->YouthsModel->index();
         
         $this->load->view('templates/header', $data);
         $this->load->view('pages/index', $data);
